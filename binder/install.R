088adbfc60bc99ecc95f4b5e9920dbe8a24dcecc
Tony Hirst <tony.hirst@gmail.com>
2020-07-21 14:40:28 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,7 +1,10 @@
 ### install regular packages
 
 install.packages("reticulate") # python support in RMarkdown
+
 install.packages("ggplot2") # for plotting
+install.packages("plotly") # for interactive plotting
+
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
 install.packages("revealjs") #presentation
