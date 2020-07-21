00a105404c1ef9b8ccac0a304b9fcc9aee02181f
Tony Hirst <tony.hirst@gmail.com>
2020-07-21 09:58:36 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -4,6 +4,10 @@ install.packages("reticulate") # python support in RMarkdown
 install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
+# Widget demos
+install.packages("leaflet") # for maps
+install.packages("dygraphs") # for timeseries
+
 ### install bioconductor packages
 # install.packages("BiocManager")
 # BiocManager::install("package")
